40989721ef9dba6ff2072f0ec75727dabee10ef2
afj8z <aidflem@gmail.com>
2025-12-08 15:15:56 +0100

    repurposing cheatsheet resource

--- src/ruby/kramdown.rb
+++
@@ -1,4 +0,0 @@
-# frozen_string_literal: true
-
-require_relative 'renderer'
-puts Renderer.render(input: $stdin.read)
--- src/styles/CommonStyles.ts
+++
@@ -1,3 +0,0 @@
-export const typeStyles = {
-  manrope: 'font-manrope font-extrabold tracking-[-0.02em]'
-}
